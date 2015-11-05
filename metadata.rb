d9cc60ef433daed1b4a43c31c0df4a6d3290e7c9
Christoph Lukas <lukas@pingworks.de>
2015-11-05 14:52:21 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'christoph.lukas@gmx.net'
 license          'All rights reserved'
 description      'Installs/Configures ws_phonebook'
 long_description 'Installs/Configures ws_phonebook'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'apt'
